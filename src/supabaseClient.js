c91367cfb2fa86026f87b05e3a3f7ae183e553cf
Sanchali Palai <palaisanchali@gmail.com>
2026-07-21 12:58:38 +0530

    fix supabase connection

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,9 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://zgitzbtmwfrxrkododml.supabase.co";
 
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
-console.log("KEY EXISTS:", !!supabaseKey);
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnaXR6YnRtd2ZyeHJrb2RvZG1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwOTg1NDQsImV4cCI6MjA5OTY3NDU0NH0.wsxgtObwNcyycuKEXpmmum3Wtk1V5-1OEoaEdzINrNc";
 
 export const supabase = createClient(
     supabaseUrl,
